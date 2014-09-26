cd88635cda8f48faae0039912c4841df20eb3b9e
Dudio <dudio.lin@thefabulabs.com>
2014-09-26 18:16:15 +0800

    9/26

---
+++ javascripts/multiple.js
@@ -0,0 +1,11 @@
+$(function(){
+	$(".multiple").each(function(){
+		var $t = $(this);
+		$t.after($('<div class="copylast">新增一筆</div>'));
+	});
+	$(".copylast").click(function(){
+		var $t = $(this);
+		$t.prev().clone(true).addClass("ondo").insertBefore($t);
+		countProperty();
+	});
+});
