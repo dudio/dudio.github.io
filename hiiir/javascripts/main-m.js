707bf677c015d298689ac79d312b3f6ba218911a
Dudio <c_dlin@groupon.com>
2015-04-15 10:56:27 +0800

    hiiir change color

--- hiiir/javascripts/main-m.js
+++ hiiir/javascripts/main-m.js
@@ -21,131 +21,39 @@ function setFont(target, color, fontSize, letterSpacing, bold, fontFamily, lineH
 	if(lineHeight) $t.css("line-height", lineHeight);
 }
 
-function setTriPosition(target, top, left, c){
-	var $t = $(target);
-	var $p = $t.parent();
-	if(c) alert((top-40)*100/$p.height()+","+left*100/$p.width());
-	var newTop = $p.offset().top+$p.height()*top/100;
-	var newLeft = $p.width()*left/100;
-	$t.css("top",newTop+"px").css("left",newLeft+"px");
-}
-
 function setDimensions(){
 	//設定各區塊高度
-	var windowWidth = $("#worldEarthDay").width();
+	var windowWidth = $("#MworldEarthDay").width();
 	var windowHeight = $(window).height();
 
-	$("#worldEarthDay").css("font-size",windowWidth/100+"px");
-	$("#topSec").height(windowWidth*0.438);
-	$("#sec1").height(windowWidth*0.548);
-	$("#sec2").height(windowWidth*0.567);
-	$("#sec3").height(windowWidth*0.498);
-	$("#sec4").height(windowWidth*0.567);
+	//$("#MworldEarthDay").css("font-size",windowWidth/100+"px");
+	$("#topSec").height(windowHeight);
+	//$("#sec1").height(windowWidth*0.548);
+	//$("#sec2").height(windowWidth*0.567);
+	//$("#sec3").height(windowWidth*0.498);
+	//$("#sec4").height(windowWidth*0.567);
 
-	$("#earth").css("top",windowWidth*15.2/100+"px");
-	$("#slogan").css("top",-windowWidth*43/100+"px");
+	var earthWidth = windowHeight*1017/931;
+	$("#earth")
+		.width(earthWidth)
+		.css("top",windowHeight*.3324)
+		.css("left",-windowHeight/200);
+	if(earthWidth > windowWidth) $("#earth").css("left", (windowWidth-earthWidth)/2-windowHeight/200);
 
-	$("#menu").css("top",windowHeight-windowWidth*0.13);
-
-	//設定小三角尺寸
-	var triWidth = windowWidth*0.0571/2;
-	$(".upTri").css("border-width","0 "+triWidth+"px "+triWidth*1.732+"px "+triWidth+"px");
-	$(".downTri").css("border-width",triWidth*1.732+"px "+triWidth+"px 0px "+triWidth+"px");
-
-	//設定小三角座標
-	setTriPosition("#upTri-1",-0.27,9.8);
-	setTriPosition("#upTri-2",22,21.286);
-	setTriPosition("#upTri-3",88.74,4.09);
-	setTriPosition("#upTri-4",10.67,75.77);
-	setTriPosition("#upTri-5",22,84.3);
-	setTriPosition("#upTri-6",88.87,84.3);
-	setTriPosition("#downTri-7",44.5,12.75);
-	setTriPosition("#downTri-8",77.88,15.57);
-	setTriPosition("#downTri-9",44.5,81.44);
-	setTriPosition("#downTri-10",66.9,75.65);
-	var l = 2.85;
-	var t = 8.9;
-	setTriPosition("#downTri-11",0,4.09);
-	setTriPosition("#downTri-12",0,4.09+l*2);
-	setTriPosition("#downTri-13",0,4.09+l*4);
-	setTriPosition("#downTri-14",t,4.09+l*1);
-	setTriPosition("#downTri-15",t,4.09+l*3);
-	setTriPosition("#downTri-16",t*2,4.09+l*2);
-	var l = 2.84;
-	var t = -8.7;
-	setTriPosition("#upTri-21",91.6,67.4);
-	setTriPosition("#upTri-22",91.6,67.4+l*2);
-	setTriPosition("#upTri-23",91.6,67.4+l*4);
-	setTriPosition("#upTri-24",91.6+t,67.4+l*1);
-	setTriPosition("#upTri-25",91.6+t,67.4+l*3);
-	setTriPosition("#upTri-26",91.6+t*2,67.4+l*2);
-	var l = 2.85;
-	var t = 10;
-	setTriPosition("#downTri-31",0,73.08);
-	setTriPosition("#downTri-32",0,73.08+l*2);
-	setTriPosition("#downTri-33",0,73.08+l*4);
-	setTriPosition("#downTri-34",t,73.08+l*1);
-	setTriPosition("#downTri-35",t,73.08+l*3);
-	setTriPosition("#downTri-36",t*2,73.08+l*2);
-	var l = -2.84;
-	var t = -8.7;
-	setTriPosition("#upTri-41",91.2,26.6);
-	setTriPosition("#upTri-42",91.2,26.6+l*2);
-	setTriPosition("#upTri-43",91.2,26.6+l*4);
-	setTriPosition("#upTri-44",91.2+t,26.6+l*1);
-	setTriPosition("#upTri-45",91.2+t,26.6+l*3);
-	setTriPosition("#upTri-46",91.2+t*2,26.6+l*2);
+	//$("#menu").css("top",windowHeight-windowWidth*0.13);
 }
 
-
 $(function(){
-
 	//添加元素
 	$("#topSec").append("<img id='earth' src='images/earth.gif'>");
-	$("#topSec").append("<br/><img id='slogan' src='images/slogan.png'>");
-	//各區塊前方的icon
-	$("#sec1 > .title").prepend("<img src='images/p1icon.png'>");
-	$("#sec2 > .title").append("<img src='images/p2icon.png'>");
-	$("#sec3 > .title").prepend("<img src='images/p3icon.png'>");
-	$("#sec4 > .title").prepend("<img src='images/p4icon.png'>");
+	$("#topSec").append("<img id='slogan' src='images/mslogan.png'>");
 	//添加buy
-	$(".product").append("<div class='buy'><div><div class='up'><div class='leftArrow'></div><div>&nbsp;BUY&nbsp;</div><div class='rightArrow'></div></div><div class='down'><div class='leftArrow'></div><div>&nbsp;BUY&nbsp;</div><div class='rightArrow'></div></div></div></div>");
-
-	//添加變色小三角
-	for(var i=1;i<=6;i++)
-		$("#topSec").append("<div class='upTri' id='upTri-"+i+"''></div>");
-	for(var i=7;i<=10;i++)
-		$("#topSec").append("<div class='downTri' id='downTri-"+i+"''></div>");
-	for(var i=11;i<=16;i++)
-		$("#sec1").append("<div class='downTri' id='downTri-"+i+"''></div>");
-	for(var i=21;i<=26;i++)
-		$("#sec2").append("<div class='upTri' id='upTri-"+i+"''></div>");
-	for(var i=31;i<=36;i++)
-		$("#sec3").append("<div class='downTri' id='downTri-"+i+"''></div>");
-	for(var i=41;i<=46;i++)
-		$("#sec4").append("<div class='upTri' id='upTri-"+i+"''></div>");
-
-
-	function changeColor(t) {
-		var $t = $(t);
-		var statu = $t.attr("statu");
-		var key = Math.floor(Math.random()*6);
-		var time = Math.random()*3000+1000;
-		colorArray = ["#d1f684","rgb(0,191,132)","rgb(0,157,130)","rgb(1,119,120)","rgb(0,97,129)","#002373"];
-		if($t.hasClass("downTri"))
-			$t.animate({borderTopColor:colorArray[key]},time,function(){changeColor(t);});
-		else
-			$t.animate({borderBottomColor:colorArray[key]},time,function(){changeColor(t);});
-	}
-
-	$(".upTri,.downTri").each(function(){
-		changeColor(this);
-	});
+	//$(".product").append("<div class='buy'><div><div class='up'><div class='leftArrow'></div><div>&nbsp;BUY&nbsp;</div><div class='rightArrow'></div></div><div class='down'><div class='leftArrow'></div><div>&nbsp;BUY&nbsp;</div><div class='rightArrow'></div></div></div></div>");
 
 	//視窗變動
 	setDimensions();
 	window.onresize=setDimensions;
-
+return;
 	//共用CSS
 	$(".sec").each(function(){
 		$(this).find(".describe").first().css("font-style","italic");
