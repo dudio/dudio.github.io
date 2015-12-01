d1e76e1ab338974b7cb99789b80d95ac52d4b610
dudio <dudiolin@gmail.com>
2015-12-01 12:04:50 +0800

    test bind onChange

--- choice-react/javascripts/Questions.js
+++ choice-react/javascripts/Questions.js
@@ -1,13 +1,15 @@
 Questions = React.createClass({
 	setWorkdayWappiness: function(){
+		console.log("AA");
 		this.props.setWorkdayWappiness(this.ref.workdayHappiness);
+		console.log("BB");
 	},
 	render: function(){
 		return (
 			<div>
 				Questions {this.props.workdayHappiness}<br/>
 				假如假日的快樂度是一百分 你覺得上班日的快樂度是幾分？(0~100)
-				<input ref="workdayHappiness" value="50" type="number" onChange="setWorkdayWappiness"/>
+				<input ref="workdayHappiness" value="50" type="number" onChange={this.setWorkdayWappiness} />
 			</div>
 		);
 	}
